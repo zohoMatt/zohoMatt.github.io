9528744d2586b9565ca7ec40a52e5633bd2c00c7
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-08-27 10:44:02 +0800

    project: Build less file structure

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by Zoho on 16/8/27.
  */
+
+/********** JS FILES *****************/
+
+
+
+/*********** LESS FILES *************/
+import '../css/main.less';
+
+/********** IMAGE FILES *************/
+
+
+
