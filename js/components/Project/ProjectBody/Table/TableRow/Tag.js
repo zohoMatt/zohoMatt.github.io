ef8a6257fde4473b1c36271e4f80b18f89483364
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-11-09 17:00:43 +0800

    feat: Redux model for add and remove project tag filter

--- js/components/Project/ProjectBody/Table/TableRow/Tag.js
+++ js/components/Project/ProjectBody/Table/TableRow/Tag.js
@@ -2,17 +2,34 @@
  * Created by Zoho on 16/9/14.
  */
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addProjectTagFilter, removeProjectTagFilter } from '../../../../../model/actions/filterAction';
 
 /**
  * @props:
  * name             {string} Tag text.
  */
+@connect(() => {
+    return {};
+})
 export default class Tag extends React.Component {
 
     render() {
         const { name, bgColor } = this.props;
         return (
-            <div className="tag" style={{ backgroundColor: bgColor }}>{name}</div>
+            <div className="tag" style={{ backgroundColor: bgColor }}
+                 onClick={this.clickHandler.bind(this)}>{name}</div>
         );
+    }
+
+    /******************** Click handler *******************/
+    clickHandler(event) {
+        const { name, clickToRemove, dispatch} = this.props;
+        if (clickToRemove == "remove") {
+            dispatch(removeProjectTagFilter(name));
+        } else {
+            dispatch(addProjectTagFilter(name));
+        }
     }
 }
