c93ad0057cb94d7ab7779fa353a0e9dc5be88aeb
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-08-16 15:39:51 +0800

    fix: Upgrade Webpack to newest edition and re-write the whole config file and it works!

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const debug = process.env.NODE_ENV !== "production";
 console.log(`=> Debug mode: ${debug ? 'on' : 'off'}.`);
 
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -15,55 +14,98 @@ module.exports = {
         'app': './js/app.js'
     },
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-                }
-            },
-            {
-                test: /\.(png|jpg|svg)(\?v=\d+\.\d+\.\d+)?/,
-                loader: 'url-loader?limit=30&name=./img/img-[hash:6].[ext]'
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?/,
-                loader: 'url-loader?limit=30&name=./font/font-[hash:6].[ext]'
-            },
+        rules: [
+            // LESS file loaders
             {
                 test: /\.less$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'style!css!autoprefixer-loader?{browsers:[">1%", "last 2 version", "Firefox 15"]}!less'
+                include: [
+                    path.resolve(__dirname, 'css')
+                ],
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 }
+                    },
+                    // {
+                    //     loader: 'postcss-loader'
+                    // },
+                    {
+                        loader: 'less-loader'
+                    }
+                ]
+            },
+
+            // File loader
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
             },
             {
-                test: /index\.html$/,
-                loader: 'file-loader?name=index.html'
+                test: /\.(htm|html)$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'index.html'
+                        }
+                    }
+                ]
+            },
+            // JavaScript files
+            {
+                test: /\.jsx?$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react', 'es2015', 'stage-0'],
+                            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+                        }
+                    }
+                ]
             }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/docs'),
+        compress: true,
+        port: 8080
     },
     output: {
         path: path.join(__dirname, 'docs'),
         filename: "[name].min.js"
     },
-    plugins: debug ? [
-        // Clean distribution folder
-        new CleanWebpackPlugin(['docs'], {
-            root: path.resolve('.'),
-            verbose: true
-        })
-    ] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-        // Clean distribution folder
-        new CleanWebpackPlugin(['docs'], {
-            root: path.resolve('.'),
-            verbose: true
-        })
-    ],
-    external: {
-    }
+    plugins: []
+    // plugins: debug ? [
+    //     // Clean distribution folder
+    //     new CleanWebpackPlugin(['docs'], {
+    //         root: path.resolve('.'),
+    //         verbose: true
+    //     })
+    // ] : [
+    //     new webpack.optimize.DedupePlugin(),
+    //     new webpack.optimize.OccurenceOrderPlugin(),
+    //     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    //     // Clean distribution folder
+    //     new CleanWebpackPlugin(['docs'], {
+    //         root: path.resolve('.'),
+    //         verbose: true
+    //     })
+    // ]
 };
