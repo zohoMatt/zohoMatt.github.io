3e33cd0e54d924a03443f187871405a864234244
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-09-30 17:05:55 +0800

    feat: Use yarn. || project: Use babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['react', 'es2015', 'stage-0'],
+                            presets: ['react', 'env', 'stage-0'],
                             plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
                         }
                     }
